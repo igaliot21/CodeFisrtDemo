91103ccd49b0671bc68ed7e263563a924df659f9
Jose Mª Izquierdo Galiot <igaliot21@gmail.com>
2020-07-12 12:23:40 +0200

    Add project files.

---
+++ Migrations/202007121016413_CreatePost.cs
@@ -0,0 +1,28 @@
+﻿namespace CodeFisrtDemo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreatePost : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Posts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        DatePublished = c.DateTime(nullable: false),
+                        Title = c.String(),
+                        BOdy = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Posts");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFisrtDemo
+{
+    public class Post { 
+        public int Id { get; set; }
+        public DateTime DatePublished { get; set; }
+        public string Title { get; set; }
+        public string BOdy { get; set; }
+        public Post() { }
+        public Post(string Title, string Body) {
+            this.Title = Title;
+            this.BOdy = Body;
+            this.DatePublished = DateTime.Now;
+        }
+    }
+    public class BlogDbContext : DbContext { 
+        public DbSet<Post> Posts { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
